e8ab4e3e3aa126c59e34980dd169964ecad39f3e
Inias Peeters <inias.peeters@axxes.com>
2025-09-26 07:29:52 +0200

    Upgrade to Java 25

--- src/main/java/eu/inias/cloudflareingressguard/CloudflareIngressGuardApplication.java
+++ src/main/java/eu/inias/cloudflareingressguard/CloudflareIngressGuardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudflareIngressGuardApplication {
 
-	public static void main(String[] args) {
+	void main(String[] args) {
 		SpringApplication.run(CloudflareIngressGuardApplication.class, args);
 	}
 
